507bae1e4ea1bee78ed635be545aee1d15114585
github username <github email address>
2019-11-19 14:49:33 -0800

    Migrate, parse data songs, and seed data

--- lib/library_parser.rb
+++ lib/library_parser.rb
@@ -28,13 +28,15 @@ class LibraryParser
   end
 
   def build_objects(artist_name, song_name, genre_name)
-    song = Song.create(name: song_name)
+    
     genre = Genre.find_or_create_by(name: genre_name)
     artist = Artist.find_or_create_by(name: artist_name)
+    song = Song.find_or_create_by(name: song_name, artist_id: artist.id, genre_id: genre.id)
 
-    song.song_genres.build(genre: genre)
-    song.artist = artist
+    # song.song_genres.build(genre: genre)
+    # song.artist = artist
     
-    song.save
+    # song.save
   end
 end
+
